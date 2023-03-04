79a090bc0122fce37e750d260d2e9ce161f270f4
Georgi-Progger <hugaeag@gmail.com>
2023-03-05 00:24:07 +0300

    new update

---
+++ src/main/resources/db/migration/V1__Create_all_skills.sql
@@ -0,0 +1,7 @@
+CREATE TABLE skills (
+  id  int(11) NOT NULL AUTO_INCREMENT,
+  name varchar(45) NOT NULL,
+  status int(4) NOT NULL,
+  PRIMARY KEY (id)
+)
+
---
+++ src/main/resources/db/migration/V4__Create_specialty_table.sql
@@ -0,0 +1,8 @@
+
+CREATE TABLE specialties (
+  id  int(11) NOT NULL AUTO_INCREMENT,
+  name varchar(45) NOT NULL,
+  status int(4) NOT NULL,
+  PRIMARY KEY (id)
+)
+
